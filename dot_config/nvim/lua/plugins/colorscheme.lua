017e5b6863d4d33fb7318e14376d2f4f3a978b98
mauricio <onestepright8@gmail.com>
2026-06-24 10:39:21 -0300

    feat: add new configs

--- dot_config/nvim/lua/plugins/colorscheme.lua
+++ dot_config/nvim/lua/plugins/colorscheme.lua
@@ -1,11 +1,36 @@
 return {
-	-- add gruvbox
-	{ "wittyjudge/gruvbox-material.nvim" },
-	-- configure LazyVim to load gruvbox
-	{
-		"LazyVim/LazyVim",
-		opts = {
-			colorscheme = "gruvbox-material",
-		},
-	},
+  {
+    "f4z3r/gruvbox-material.nvim",
+    name = "gruvbox-material",
+    lazy = false,
+    priority = 1000,
+
+    opts = {
+      italics = true,
+      contrast = "soft",
+
+      comments = {
+        italics = true,
+      },
+
+      background = {
+        transparent = false,
+      },
+
+      float = {
+        force_background = false,
+        background_color = nil,
+      },
+
+      signs = {
+        force_background = false,
+        background_color = nil,
+      },
+    },
+
+    config = function(_, opts)
+      require("gruvbox-material").setup(opts)
+      vim.cmd.colorscheme("gruvbox-material")
+    end,
+  },
 }
